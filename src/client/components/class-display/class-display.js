3f1d9e0cd33f4c97305673b64a04ddb5f9a507a9
Tom Corke <tom@tomcorke.com>
2018-04-28 13:46:57 +0100

    Filter character display to only guild members

--- src/client/components/class-display/class-display.js
+++ src/client/components/class-display/class-display.js
@@ -4,12 +4,21 @@ import WowCharacter from '../wow-character'
 
 import STYLES from './class-display.scss'
 
+const guildFilter = (char) => {
+  return (
+    char.guild === 'Distinctly Average' &&
+    char.realm === 'Silvermoon'
+  )
+}
+
 const ClassDisplay = ({ characters }) => {
+  const guildCharacters = characters.filter(guildFilter)
+
   return (
     <div className={STYLES.classDisplay}>
       <div className={STYLES.blurb}>Your guild characters:</div>
       <div className={STYLES.characters}>
-        {characters.map(c =>
+        {guildCharacters.map(c =>
           <WowCharacter key={`${c.name}-${c.realm}`} character={c} />)}
       </div>
     </div>
--- src/client/config/development.js
+++ src/client/config/development.js
@@ -3,7 +3,7 @@ const config = {
   bnetAuthEndpoint: 'https://localhost:3443/auth/bnet',
   saveDataEndpoint: 'https://localhost:3443/save',
 
-  _mockUserData: {
+  mockUserData: {
     user: {
       battletag: 'Shot#2975',
       id: 999,
@@ -25,6 +25,13 @@ const config = {
           class: 'warrior',
           level: 120,
           guild: 'Distinctly Average',
+          realm: 'Silvermoon'
+        },
+        {
+          name: 'TestCharacter2',
+          class: 'priest',
+          level: 120,
+          guild: 'Just Awful',
           realm: 'Silvermoon'
         }
       ]
