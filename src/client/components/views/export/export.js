0d3af99ed2070affe0cd099d4c651a620bc072f5
Tom Corke <tom@tomcorke.com>
2018-05-06 02:31:18 +0100

    fix export view (coming soon still)

--- src/client/components/views/export/export.js
+++ src/client/components/views/export/export.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import STYLES from './export.scss'
 
-const ExportView = <div className={STYLES.export}>export coming soon</div>
+const ExportView = () => <div className={STYLES.export}>export coming soon</div>
 
 export default ExportView
