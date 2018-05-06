23d24c18cb334f43e4fb7a6d436559797c7e50d8
Tom Corke <tom@tomcorke.com>
2018-05-06 02:32:25 +0100

    Remove most of the now re-implemented code from root planner component

--- src/client/components/bfa-planner/bfa-planner.js
+++ src/client/components/bfa-planner/bfa-planner.js
@@ -12,113 +12,6 @@ import WindowMessageReceiver from '../window-message-receiver'
 import STYLES from './bfa-planner.scss'
 
 /*
-const popupWindow = (url, win, w, h) => {
-  var y = win.top.outerHeight / 2 + win.top.screenY - (h / 2)
-  var x = win.top.outerWidth / 2 + win.top.screenX - (w / 2)
-  return win.open(url, '_blank', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + y + ', left=' + x)
-}
-
-class BfaPlannerOld extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.receiveMessage = this.receiveMessage.bind(this)
-    this.onLogin = this.onLogin.bind(this)
-    this.onSave = this.onSave.bind(this)
-    this.onChoiceChanged = this.onChoiceChanged.bind(this)
-    this.onHideFeedbackMessage = this.onHideFeedbackMessage.bind(this)
-    this.onViewMenuClick = this.onViewMenuClick.bind(this)
-  }
-
-  onLogin () {
-    const { bnetAuthEndpoint } = this.props.config
-    const authUrl = bnetAuthEndpoint
-    this.authWindow = popupWindow(authUrl, window, 450, 600)
-  }
-
-  onSave () {
-    const xhr = new window.XMLHttpRequest()
-    const { saveDataEndpoint } = this.props.config
-    xhr.open('POST', saveDataEndpoint, true)
-    xhr.setRequestHeader('content-type', 'application/json')
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        this.setState({
-          ...this.state,
-          hasChanges: false
-        })
-        setImmediate(() => this.showFeedbackMessage('Saved!'))
-      }
-    }
-    xhr.send(JSON.stringify(this.state.data))
-  }
-
-  receiveMessage (event) {
-    if (event.source === this.authWindow) {
-      console.log('Received message from auth window', event.data)
-      this.handleUserData(event.data.userData)
-      this.authWindow.close()
-
-      if (
-        this.state.isLoggedIn &&
-        !this.state.hasCharacters
-      ) {
-        this.props.getUserData()
-      }
-    }
-  }
-
-  componentWillMount () {
-    this.props.getUserData()
-    window.addEventListener('message', this.receiveMessage, false)
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('message', this.receiveMessage, false)
-  }
-
-  onChoiceChanged (name, prop, value) {
-    const choice = this.state.data[name] || {}
-    this.setState({
-      ...this.state,
-      data: {
-        ...this.state.data,
-        [name]: {
-          ...choice,
-          [prop]: value
-        }
-      },
-      hasChanges: true
-    })
-  }
-
-  onHideFeedbackMessage () {
-    window.clearTimeout(this.fadeOutFeedbackMessageTimer)
-    window.clearTimeout(this.hideFeedbackMessageTimer)
-    this.setState({
-      ...this.state,
-      showFeedbackMessage: false
-    })
-  }
-
-  fadeOutFeedbackMessage () {
-    window.clearTimeout(this.fadeOutFeedbackMessageTimer)
-    this.setState({
-      ...this.state,
-      fadeOutFeedbackMessage: true
-    })
-    this.hideFeedbackMessageTimer = window.setTimeout(() => this.onHideFeedbackMessage(), 1000)
-  }
-
-  showFeedbackMessage (message) {
-    this.setState({
-      ...this.state,
-      showFeedbackMessage: true,
-      fadeOutFeedbackMessage: false,
-      feedbackMessage: message
-    })
-    this.fadeOutFeedbackMessageTimer = window.setTimeout(() => this.fadeOutFeedbackMessage(), 1000)
-  }
 
   tryChangeView (option) {
     if (option === 'main') {
@@ -150,13 +43,6 @@ class BfaPlannerOld extends React.Component {
         })
         .catch(err => console.error(`Error fetching overview view data: ${err.message}`))
     }
-  }
-
-  onViewMenuClick (option) {
-    this.tryChangeView(option)
-  }
-
-  render () {
   }
 }
 */
