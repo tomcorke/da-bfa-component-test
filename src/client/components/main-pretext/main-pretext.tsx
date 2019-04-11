f4f2e0409005966ca18de4b1a5b93e0202ecb9f9
Tom Corke <tom.corke@skyscanner.net>
2019-04-11 21:49:48 +0100

    Update pretext removing references to releasing site early

--- src/client/components/main-pretext/main-pretext.tsx
+++ src/client/components/main-pretext/main-pretext.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 const MainPretext = () => (
   <p>
-    Welcome to class selection! We're releasing this a little early so you can get to grips with this page, and we can get an idea who is looking at what for the expansion! - Any problems and we'll contact you directly! Have fun!
+    Welcome to class selection! Declare your class and spec for Disinctly Average's raid team here.
   </p>
 )
 
